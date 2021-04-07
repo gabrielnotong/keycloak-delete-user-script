8aaf23fe1ff3e879bcfb2d4ffc11bc5bfdcfbd4a
gnotong <g.notong@ubitransport.com>
2021-04-07 11:05:13 +0200

    updating files

--- delete_users.php
+++ delete_users.php
@@ -3,7 +3,7 @@
 require_once './ExUbiUsersCleaner.php';
 
 $exUbiUsersCleaner = new ExUbiUsersCleaner(
-    __DIR__ . DIRECTORY_SEPARATOR . 'ex_ubi_users_keycloak_next2.csv',
+    __DIR__ . DIRECTORY_SEPARATOR . 'csv' . DIRECTORY_SEPARATOR . 'ex_ubi_users_keycloak_next2.csv',
     'https://authentification.prod.service.2cloud.app/auth/realms/master/protocol/openid-connect/token'
 );
 
